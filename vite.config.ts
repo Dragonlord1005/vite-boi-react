dbc2c1eaec3ba546c5651be49af5b0b24b627036
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-08 01:55:08 +0000

    Unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unocss from 'unocss/vite'
+import presetAttributify from '@unocss/preset-attributify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    Unocss({
+      presets: [
+        presetAttributify({})
+      ],
+    }
+    ),
+  ]
 })
