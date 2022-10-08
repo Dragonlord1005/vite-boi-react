cd78935f9de9f549b7d3585c75e474767fa1c59f
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-08 04:03:22 +0000

    wow

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="max-width-1280px margin-0 margin-auto">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="p-2em">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-hex-888888">
         Click on the Vite and React logos to learn more
       </p>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Unocss from 'unocss/vite'
-import presetAttributify from '@unocss/preset-attributify'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    Unocss({
-      presets: [
-        presetAttributify({})
-      ],
-    }
-    ),
+    WindiCSS(),
   ]
 })
